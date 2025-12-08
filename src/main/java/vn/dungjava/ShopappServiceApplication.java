c34bc6554c43f750e042645855cad3d56a9e873e
DungHoang <hoangdung1412003@gmail.com>
2025-12-09 02:02:18 +0700

    First commit

---
+++ src/main/java/vn/dungjava/ShopappServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.dungjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopappServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopappServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/dungjava/ShopappServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.dungjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopappServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
